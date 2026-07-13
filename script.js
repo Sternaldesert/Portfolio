0dd64b9e378e5882661718949b1766df8fd3ec6f
Sternaldesert <90590690+Sternaldesert@users.noreply.github.com>
2026-07-13 13:22:07 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const menuBtn = document.querySelector('#menu-btn');
-const menu = document.querySelector('#menu');
-
-menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
-});
+function toggleSidebar(isOpen) {
+  const sidebar = document.getElementById('Mobile-menu');
+  const backdrop = document.getElementById('Mobile-menu-blur');
+  
+  if (isOpen) {
+    // Add tailwind classes on the fly
+    sidebar.classList.remove('translate-x-full');
+    backdrop.classList.remove('hidden');
+  } else {
+    sidebar.classList.add('translate-x-full');
+    backdrop.classList.add('hidden');
+  }
+}
